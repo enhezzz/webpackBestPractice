9879f61a41d3f0af537aa318ec191cb503d8edc2
enhezzz <13450737557@163.com>
2018-03-29 23:28:01 +0800

    指南

---
+++ 指南/开发/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'                     //使用 webpack-dev-middleware时开启
+    }
+};
---
+++ 指南/模块热替换/src/index.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import printMe from './print.js';
+import './style.css';
+function component() {
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
+    element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    return element;
+}
+document.body.appendChild(component());
+
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        document.querySelector('button').onclick = printMe
+    })
+}
